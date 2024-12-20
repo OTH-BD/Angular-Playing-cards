6586c46fa90101e35cc6325b42ab14951a437adb
Boudali Othmane <109677961+OTH-BD@users.noreply.github.com>
2024-12-20 02:02:08 +0100

    v1

---
+++ src/app/components/playing-card/playing-card.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playing-card',
+  imports: [],
+  templateUrl: './playing-card.component.html',
+  styleUrl: './playing-card.component.css'
+})
+export class PlayingCardComponent {
+
+  name: string = "My Monster";
+  hp : number = 40 ; 
+  figureCaption: string ="N°001 Monster"
+  attackName: string= "Geo Impact";
+  attackStrength: number = 60;
+  attackDescription : string ="This is a long description............";
+
+
+}
